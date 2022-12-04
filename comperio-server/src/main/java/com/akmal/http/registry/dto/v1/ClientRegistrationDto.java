7b4acce5c62a973a3cc34598481d76528ec39211
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-12-04 15:14:10 +0100

    Implement ClientService to abstract the registry interaction.

---
+++ comperio-server/src/main/java/com/akmal/http/registry/dto/v1/ClientRegistrationDto.java
@@ -0,0 +1,27 @@
+package com.akmal.http.registry.dto.v1;
+
+import com.akmal.registry.ClientRegistration;
+
+public record ClientRegistrationDto(
+  String application,
+  String instanceId,
+  String host,
+  int port,
+  String ipAddress,
+  long registrationTimestamp,
+  long lastRenewalTimestamp,
+  long renewalsSinceRegistration,
+  String status
+) {
+
+  public static ClientRegistrationDto fromClientRegistration(ClientRegistration clientRegistration) {
+    return new ClientRegistrationDto(clientRegistration.application(),
+        clientRegistration.instanceId(), clientRegistration.host(),
+        clientRegistration.port(),
+        clientRegistration.ipAddress() != null ? clientRegistration.ipAddress().toString() : null,
+        clientRegistration.registrationTimestamp(),
+        clientRegistration.lastRenewalTimestamp(),
+        clientRegistration.renewalsSinceRegistration(),
+        clientRegistration.status().name());
+  }
+}
